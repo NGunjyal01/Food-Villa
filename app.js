522a28a6149358f906dc1fb32e9b9fac24b9dc28
NGunjyal01 <127893749+NGunjyal01@users.noreply.github.com>
2023-08-22 00:48:00 +0530

    Add files via upload
    basic files created

---
+++ app.js
@@ -0,0 +1,20 @@
+/**
+ * header
+ *      logo
+ *      nav items(right side)
+ *      cart
+ * body
+ *      search bar
+ *      resturant list
+ *      resturant card
+ *          image
+ *          name
+ *          rating
+ *          cusines
+ * footer
+ *      links
+ *      copyrightWS
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
