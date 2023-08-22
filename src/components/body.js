7d870ef778a9668314a87aa1ebe85bc75b9041f1
NGunjyal01 <ngunjyal25@gmail.com>
2023-08-22 16:54:41 +0530

    Update body.js
    done all the things from class 6, learn about useState and showing data according to search input but searches are case sensitive

--- src/components/body.js
+++ src/components/body.js
@@ -1,13 +1,40 @@
 import { ResturantList } from "../constans";
 import ResturantCard from "./ResturantCard";
+import { useState } from "react";
+
+function filterData(searchInput, resturants){
+    const Data= resturants.filter((resturant)=>{
+        return resturant.info.name.includes(searchInput)
+    });
+    return Data;
+}
 
 const Body = () => {
+    const [resutrants, setResturants]= useState(ResturantList);
+    const [searchInput, setSearchInput]=useState();
+
     return (
-        <div className="resturant-list">
-            {ResturantList.map(resturant => {
-                return (<ResturantCard {...resturant.info} key={resturant.info.id} />);
-            })}
-        </div>
+        <>
+            <div className="search-container">
+                <input
+                    type='text' className="search-input" placeholder="search"
+                    onChange={(e) =>{
+                       setSearchInput(e.target.value);
+                    }}
+                />
+                <button className="search-btn"
+                    onClick={()=>{
+                        const Data= filterData(searchInput, resutrants);
+                        setResturants(Data);
+                    }}
+                >search</button>
+            </div>
+            <div className="resturant-list">
+                {resutrants.map(resturant => {
+                    return (<ResturantCard {...resturant.info} key={resturant.info.id} />);
+                })}
+            </div>
+        </>
     );
 };
 
