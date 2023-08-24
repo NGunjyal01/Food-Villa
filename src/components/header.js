c58ea4f7ce1bff766e198a1a371d176095a38f18
NGunjyal01 <ngunjyal25@gmail.com>
2023-08-24 17:50:58 +0530

    add new feature of login and logout and it changes on each click

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Title = () => {
     return (
         <img alt="logo" className="logo" src="https://cdn.octopix.in/uploads/company-logo/2020/11/19/food-villa-pSJVhwoN8KxgwV9jtuB1MlosJ0ejoKfiBiVO1jJPLM61shyarbxVvjIFy3DVpbUML8eBxcUo7BOWXQcd-350x350.jpg"></img>
@@ -5,6 +7,9 @@ const Title = () => {
 };
 
 const Header = () => {
+    
+    const [isLogedIn, setIsLogedIn] = useState(false);
+
     return (
         <div className="header">
             <Title />
@@ -16,6 +21,8 @@ const Header = () => {
                     <li>cart</li>
                 </ul>
             </div>
+            {isLogedIn ? <button onClick={ () => setIsLogedIn(false) }>Log Out</button> : 
+            <button onClick={ () => setIsLogedIn(true) }>Log In</button>}
         </div>
     );
 };
