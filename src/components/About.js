475bab73153a56813bfe97829131cb1134c06bee
NGunjyal01 <127893749+NGunjyal01@users.noreply.github.com>
2023-08-31 00:56:58 +0530

    Done all things till class 11

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const About = () => {
+    return (
+        <>
+            <h1>about us</h1>
+            <Outlet/>
+        </>
+    );
+};
+
+export default About;
